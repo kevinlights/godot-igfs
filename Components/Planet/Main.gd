0a10f311226bde5d5a38d9a956ff10640994c3b5
pjtsearch <pjtsignups@gmail.com>
2019-07-28 08:50:45 -0700

    make planet component, make each star texture unique, so not the same on every star

--- Components/Planet/Main.gd
+++ Components/Planet/Main.gd
@@ -1,9 +1,11 @@
 extends StaticBody
 
+export(Texture) var planet_texture setget set_planet_texture
+# export(Number) var planet_radius setget set_planet_radius
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export(Texture) var star_texture setget set_star_texture
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,9 +14,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-func set_star_texture(texture):
+func set_planet_texture(texture):
+	# var material = ResourceLoader.load("res://Components/Planet/materials/material.tres");
 	var material = get_node("MeshInstance").get_surface_material(0).duplicate()
 	# print(material.emission_texture)
-	material.emission_texture = texture
+	material.albedo_texture = texture
 
 	get_node("MeshInstance").set_surface_material(0,material)
+	
+
+func set_planet_radius(radius):
+	pass
