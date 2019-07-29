885d0bfbee8c700295eeffef0157fd329e13b54a
pjtsearch <pjtsignups@gmail.com>
2019-07-29 11:53:22 -0700

    add multiplayer connections, SpaceShipPuppet adding to scenes for each player

---
+++ Components/SpaceShipPuppet/Main.gd
@@ -0,0 +1,163 @@
+extends KinematicBody
+
+var _initial_position
+var _initial_rotation
+var _rot_x = 0
+var _rot_y = 0
+var _rot_z = 0
+var _heat_interval = false
+var health = 100
+# change to ship in storage
+var SHIP_TURN_RATE = 0.05
+
+signal position(content) 
+
+var speed = 0.1
+
+func _ready():
+#    set_physics_process(true)
+#    set_gravity_scale(1)
+    # pass
+    _initial_position = get_global_transform().origin
+    _initial_rotation = get_global_transform().basis
+    addConnections()
+    # while ( true ):
+    #     yield( get_tree().create_timer(0.4), "timeout" )
+    #     # EventManager.emit("ship_position",translation)       
+    #     emit_signal("position",translation) 
+
+# func _physics_process(delta):
+#     var bodies = get_colliding_bodies()
+
+#     for curBody in bodies:
+#         print( "collision :" + curBody.get_name() )
+
+#     if Input.is_key_pressed(KEY_E):
+#         speed = speed + .1
+#     if Input.is_key_pressed(KEY_Q):
+#         speed = speed - .1
+#     if Input.is_key_pressed(KEY_S):
+#         rotate_object_local(Vector3(1, 0, 0), delta/.8)
+#     if Input.is_key_pressed(KEY_W):
+#         rotate_object_local(Vector3(1, 0, 0), -delta/.8)
+#     if Input.is_key_pressed(KEY_A):
+#         rotate_object_local(Vector3(0, 1, 0), delta/.8)
+#     if Input.is_key_pressed(KEY_D):
+#         rotate_object_local(Vector3(0, 1, 0), -delta/.8)
+
+#     translate(Vector3(0,0,delta*speed))
+
+func addConnections():
+    # get_node("HeatArea").connect("body_entered", self, "heat_body_enter")
+    # get_node("HeatArea").connect("body_exited", self, "heat_body_exit")
+    pass
+
+func _enter_tree():
+    # Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+    pass
+
+func _exit_tree():
+    # Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE);
+    pass
+
+func _unhandled_input(event):
+    # if event is InputEventKey:
+    #     if event.pressed and event.scancode == KEY_R:
+    #         reset_ship()
+    pass
+            
+
+func _process(delta):
+    # var collisionInfo = move_and_slide(global_transform.basis.z * -1 * speed)
+    # emit_signal("position",{"coords":translation,"rotation":get_rotation(),"health":health}) 
+
+    # if (health <= 0):
+    #     reset_ship()
+
+    # var array_stars = get_tree().get_nodes_in_group("star")
+    # print(global_transform)
+    # for star in array_stars:
+    #     var distance_to_star = global_transform.distance_to(star.global_transform)
+    #     if distance_to_star < 200:
+    #         print("n")
+
+    # if get_slide_count():
+    #     # print(get_slide_collision(0).get_collider().get_name())
+    #     if abs(speed) > 18:
+    #         crash()
+
+    # var TURN_SPEED = 0
+    # # var SHIP_TURN_RATE_RECIP = 1/SHIP_TURN_RATE
+    # if abs(speed * SHIP_TURN_RATE) <= SHIP_TURN_RATE * 10:
+    #     TURN_SPEED = delta * abs(speed * SHIP_TURN_RATE)
+    # else:
+    #     TURN_SPEED = delta * (SHIP_TURN_RATE * 10)
+
+    # if Input.is_key_pressed(KEY_UP):
+    #     speed = speed + 1
+    # if Input.is_key_pressed(KEY_DOWN):
+    #     speed = speed - 1
+    # if Input.is_key_pressed(KEY_S):
+    #     rotate_object_local(Vector3(1, 0, 0), TURN_SPEED)
+    #     _rot_x += TURN_SPEED
+    # if Input.is_key_pressed(KEY_W):
+    #     rotate_object_local(Vector3(1, 0, 0), -TURN_SPEED)
+    #     _rot_x -= TURN_SPEED
+    # if Input.is_key_pressed(KEY_A):
+    #     rotate_object_local(Vector3(0, 1, 0), TURN_SPEED)
+    #     _rot_y += TURN_SPEED
+    # if Input.is_key_pressed(KEY_D):
+    #     rotate_object_local(Vector3(0, 1, 0), -TURN_SPEED)
+    #     _rot_y -= TURN_SPEED
+    # if Input.is_key_pressed(KEY_Q):
+    #     rotate_object_local(Vector3(0, 0, 1), TURN_SPEED)
+    #     _rot_z += TURN_SPEED
+    # if Input.is_key_pressed(KEY_E):
+    #     rotate_object_local(Vector3(0, 0, 1), -TURN_SPEED)
+    #     _rot_z -= TURN_SPEED
+    pass
+
+func reset_ship():
+    # set_translation(_initial_position)
+    # speed = 0.1
+    # set_rotation(Vector3(0, 0, 0))
+    # yield( get_tree().create_timer(0.05), "timeout" )
+    # health = 100
+    pass
+# func _integrate_forces(state):
+    # var a = state.get_transform().basis
+    # set_linear_velocity(Vector3(a.x.z, a.y.z, -a.z.z) * speed)
+    # if Input.is_key_pressed(KEY_Z):
+    #     set_linear_velocity(Vector3(1,1,1) * 0.00001)
+    #     set_angular_velocity(Vector3(1,1,1) * 0.00001)
+
+    # move_and_slide(Vector3(a.x.z, a.y.z, -a.z.z) * speed)
+
+func crash():
+    # print("crashed")
+    # global_event_bus.publish("message",{"text":"Crashed!"})
+    # emit_signal("message","crashed")
+    # health = 0
+    # EventManager.emit("message",{"text":"Crashed!"})
+    pass
+    # get_node("../Message/Message").set_text("Crashed")
+    # emit_signal("message","Crashed")
+    # print(get_node("../Message/Message"))
+    # emit_signal("message","Crashed")
+    
+    # THIS WAS THE PROBLEM
+    # get_tree().reload_current_scene()
+
+
+func heat_body_enter(body):
+    # if body.get_groups().has("star"):
+    #     _heat_interval = true
+    #     while ( _heat_interval == true ):
+    #         yield( get_tree().create_timer(0.05), "timeout" )
+    #         health = health - 1
+    pass
+            # print(health)
+
+func heat_body_exit(body):
+    # _heat_interval = false
+    pass
--- Scenes/Game/Main.gd
+++ Scenes/Game/Main.gd
@@ -1,16 +1,103 @@
 extends Spatial
 
+
+var HOST = true
+
+# Player info, associate ID to data
+var player_info = {}
+# Info we send to other players
+var my_info = { name = "PJT" }
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	if HOST:
+		var peer = NetworkedMultiplayerENet.new()
+		peer.create_server(5000, 10)
+		get_tree().set_network_peer(peer)
+	else:
+		var peer = NetworkedMultiplayerENet.new()
+		peer.create_client("127.0.0.1", 5000)
+		get_tree().set_network_peer(peer)
+	print("Peer: " + str(get_tree().get_network_peer()))
+	print("Is server: " + str(get_tree().is_network_server()))
+	add_connections()
 
+func add_connections():
+	get_tree().connect("network_peer_connected", self, "player_connected")
+	get_tree().connect("network_peer_disconnected", self, "player_disconnected")
+	get_tree().connect("connected_to_server", self, "connected_ok")
+	get_tree().connect("connection_failed", self, "connected_fail")
+	get_tree().connect("server_disconnected", self, "server_disconnected")
 
+func player_connected(id):
+	print("Player connected, id: " + str(id))
 
+func player_disconnected(id):
+	print("Player disconnected, id: " + str(id))
+
+	rpc("unregister_player", id)
+	unregister_player(id)
+
+func connected_ok():
+	print("Connected to server")
+	# Only called on clients, not server. Send my ID and info to all the other peers.
+	rpc("register_player", get_tree().get_network_unique_id(), my_info)
+
+func connected_fail():
+	print("Failed to connect to server")
+
+func server_disconnected():
+	print("Disconnected from server")
+	
 # func _on_message(message):
 # 	print("message: " + message)
 # 	get_node("Message").text = message
 # 	print(get_node("Message").text)
+
+remote func register_player(id, info):
+	# Store the info
+	player_info[id] = info
+	var player = preload("res://Components/SpaceShipPuppet/SpaceShipPuppet.tscn").instance()
+	# var player = CSGBox.new()
+	player.set_name(str(id))
+	player.set_network_master(id) # Will be explained later
+	get_node("SpaceShips").add_child(player)
+	# If I'm the server, let the new guy know about existing players.
+	if get_tree().is_network_server():
+		# Send my info to new player
+		rpc_id(id, "register_player", 1, my_info)
+		# Send the info of existing players
+		for peer_id in player_info:
+			rpc_id(id, "register_player", peer_id, player_info[peer_id])
+
+	print("Players: " + str(player_info))
+
+remote func unregister_player(id):
+	# Store the info
+	player_info.erase(id)
+
+	get_node("SpaceShips/" + str(id)).queue_free()
+
+	print("Players: " + str(player_info))
+
+
+# remote func pre_configure_game():
+# 	var selfPeerID = get_tree().get_network_unique_id()
+
+# 	# Load my player
+# 	get_node("SpaceShip").set_network_master(selfPeerID)
+# 	# get_node("SpaceShip").set_name(str(selfPeerID))
+
+# 	# Load other players
+# 	for p in player_info:
+# 		var player = preload("res://Components/SpaceShip/SpaceShip.tscn").instance()
+# 		player.set_name(str(p))
+# 		player.set_network_master(p) # Will be explained later
+# 		get_node("SpaceShips").add_child(player)
+
+	# Tell server (remember, server is always ID=1) that this peer is done pre-configuring.
+	# rpc_id(1, "done_preconfiguring", selfPeerID)
